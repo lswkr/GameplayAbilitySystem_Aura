1a355d8dc79354bcfdb7eca371b6380f7d92ed11
LSW <tmddnjs2056@naver.com>
2025-06-11 00:01:55 +0900

    Attribute Accessors

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -4,8 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "AuraAttributeSet.generated.h"
 
+
+//ATTRIBUTE_ACCESSORS 한 줄로 아래 4개의 매크로를 다 설정
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 /**
  * 
  */
@@ -20,16 +29,20 @@ public:
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes") //ReplicatedUsing는 레플리케이트 되도록 하는 rep notify함수(OnRep_~는 언리얼엔진 전통)
 	FGameplayAttributeData Health;
-
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Health);
+	
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
 	FGameplayAttributeData MaxHealth;
-
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxHealth);
+	
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
 	FGameplayAttributeData Mana;
-
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Mana);
+	
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attributes")
 	FGameplayAttributeData MaxMana;
-
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxMana);
+	
 	UFUNCTION()//rep notify함수라 UFUNCTION해줘야함
 	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
 
